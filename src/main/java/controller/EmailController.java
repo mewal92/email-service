23fb28807aeb61b1d0312affea51f371558338d6
Melinda Walter <melinda_walter@hotmail.com>
2024-05-20 15:08:22 +0200

    pub sub push

--- src/main/java/controller/EmailController.java
+++ src/main/java/controller/EmailController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import lombok.Getter;
 import model.EmailRequests;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import services.EmailService;
 
 import javax.mail.MessagingException;
+import java.util.Base64;
 
 @RestController
 public class EmailController {
@@ -27,4 +30,39 @@ public class EmailController {
             return ResponseEntity.status(500).body("Failed to send email");
         }
     }
+
+    @PostMapping("/pubsub/push")
+    public ResponseEntity<String> receiveMessage(@RequestBody PubSubMessage message) {
+        String data = new String(Base64.getDecoder().decode(message.getMessage().getData()));
+        String[] parts = data.split(",");
+        String email = parts[0].split("=")[1];
+        String subject = "Bokningsbekräftelse";
+        String body = "Tack för din bokning på BookingBee. Detaljer: " + data;
+
+        try {
+            emailService.sendEmail(email, subject, body);
+            return new ResponseEntity<>("Email sent successfully", HttpStatus.OK);
+        } catch (MessagingException e) {
+            return new ResponseEntity<>("Failed to send email", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Getter
+    public static class PubSubMessage {
+        private Message message;
+
+        public void setMessage(Message message) {
+            this.message = message;
+        }
+
+        @Getter
+        public static class Message {
+            private String data;
+
+            public void setData(String data) {
+                this.data = data;
+            }
+        }
+    }
 }
+
